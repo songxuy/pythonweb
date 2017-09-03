82903bd37236a5a32dc05d2f5e438e99dedd6998
songxuy <541046145@qq.com>
2017-09-03 19:58:35 +0800

    app

---
+++ www/app.py
@@ -0,0 +1,38 @@
+'''import asyncio,os,json,time
+import logging; logging.basicConfig(level=logging.INFO)
+from datetime import datetime
+from aiohttp import web
+def index(request):
+	return web.Response(body=b'<h1>Awesome</h1>')
+@asyncio.coroutine
+def init(loop):
+	app=web.Application(loop=loop)
+	app.router.add_route('GET','/',index)
+	srv=await loop.create_server(app.make_handler(),'127.0.0.1',9000)
+	logging.info('server started at http://127.0.0.1:9000...')
+	return srv
+loop=asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()'''
+__author__ = 'Michael Liao'
+
+'''
+async web application.
+'''
+
+import logging; logging.basicConfig(level=logging.INFO)
+import asyncio, os, json, time
+from aiohttp import web
+def index(request):
+    return web.Response(body=b'<h1> Awesome </h1>', content_type='text/html')
+
+async def init(loop): # async 替代 @asyncio.coroutine装饰器,代表这个是要异步运行的函数
+    app=web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = await loop.create_server(app.make_handler(), '0.0.0.0', 5000)  #await 代替yield from ,表示要放入asyncio.get_event_loop中进行的异步操作
+    logging.info('server started at http://127.0.0.1:5000...')
+    return srv
+
+loop = asyncio.get_event_loop()  #创建asyncio event loop 
+#loop.run_until_complete(init(loop)) #用asyncio event loop 来异步运行init()
+#loop.run_forever()
